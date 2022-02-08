88f6cb2e50157ecfa341ed1f98b4d6890e6281bc
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2022-02-08 16:36:58 +0100

    Update dependencies
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- jee9/build.gradle.kts
+++ jee9/build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
   implementation("org.openjdk.jmh:jmh-core:1.34")
   annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.34")
 
-  compileOnly("org.checkerframework:checker-qual:3.21.1")
-  testCompileOnly("org.checkerframework:checker-qual:3.21.1")
+  compileOnly("org.checkerframework:checker-qual:3.21.2")
+  testCompileOnly("org.checkerframework:checker-qual:3.21.2")
 
   implementation("com.google.guava:guava:31.0.1-jre")
   implementation("jakarta.validation:jakarta.validation-api:3.0.1")
@@ -69,5 +69,5 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("com.google.truth.extensions:truth-java8-extension:1.1.3")
-  testImplementation("nl.jqno.equalsverifier:equalsverifier:3.8.3")
+  testImplementation("nl.jqno.equalsverifier:equalsverifier:3.9")
 }
